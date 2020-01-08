20f07474c1cdebd321bae55bb8d516d9113a18d7
Jungwoo <westhyena@gmail.com>
2020-01-08 21:08:30 +0900

    train/valid 스코어 둘다 찍게

--- docker/src/train.py
+++ docker/src/train.py
@@ -50,30 +50,37 @@ model.train(data_loader.get_train_data(), data_loader.get_valid_data(),
             epochs=10, batch_size=32,
             callbacks=callbacks)
 
-# Valid F1 score가 가장 잘나오는 베스트 
 
+def print_score(data_x, data_y, data_type):
+  y_pred = model.predict(data_x)
+  f1_list = []
+  thr_list = []
+  for thr in np.linspace(0, 1, 100):
+    f1_list.append(f1_score(data_y, y_pred > thr))
+    thr_list.append(thr)
+
+  thr_idx = np.argmax(f1_list)
+  score_f1 = np.max(f1_list)
+  score_auroc = roc_auc_score(data_y, y_pred)
+  print("Best", data_type, "F1", score_f1, thr_list[thr_idx])
+  print(data_type, "AUROC", score_auroc)
+  
+  if score_auroc + score_f1 == 0:
+    score = 0
+  else:
+    score = 2 * score_auroc * score_f1 / (score_auroc + score_f1)
+  print(data_type, "Score", score)
+
+  return f1_list, thr_list
+
+
+train_x, train_y = data_loader.get_train_data()
 valid_x, valid_y = data_loader.get_valid_data()
-y_pred = model.predict(valid_x)
 
-f1_list = []
-thr_list = []
-for thr in np.linspace(0, 1, 100):
-  f1_list.append(f1_score(valid_y, y_pred > thr))
-  thr_list.append(thr)
+train_f1, train_thr = print_score(train_x, train_y, 'Train')
+valid_f1, valid_thr = print_score(valid_x, valid_y, 'Valid')
 
-thr_idx = np.argmax(f1_list)
-score_f1 = np.max(f1_list)
-score_auroc = roc_auc_score(valid_y, y_pred)
-print("Best Valid F1", score_f1, thr_list[thr_idx])
-print("Valid AUROC", score_auroc)
-
-if score_auroc + score_f1 == 0:
-  score = 0
-else:
-  score = 2 * score_auroc * score_f1 / (score_auroc + score_f1)
-print("Valid Score", score)
-
-np.save(os.path.join(task_path, 'f1.npy'), f1_list)
-np.save(os.path.join(task_path, 'thr.npy'), thr_list)
+np.save(os.path.join(task_path, 'f1.npy'), train_f1)
+np.save(os.path.join(task_path, 'thr.npy'), train_thr)
 
 print("Train Done")

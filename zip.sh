b6ed595865dad0188a24cd73f83f431571044716
hurcy <cinyoung.hur@gmail.com>
2020-01-09 15:58:20 +0900

    Add faster zip command

--- zip.sh
+++ zip.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
-
+FASTER=${1:-0}
 CODE="$(tr -dc '[:alnum:]' </dev/urandom | head -c 6)"
 DOCKERZIP="prism_${CODE}.tar.gz"
 echo $DOCKERZIP
-docker save prism-tensorflow-1.14.0-gpu-py3 | gzip -c -v > $DOCKERZIP
+if [ $FASTER = "-f" ] 
+then
+   echo "faster compression using multi-core"
+   docker save prism-tensorflow-1.14.0-gpu-py3 | pigz -c -v > $DOCKERZIP
+else
+   echo "normal compression"
+   docker save prism-tensorflow-1.14.0-gpu-py3 | gzip -c -v > $DOCKERZIP
+fi
